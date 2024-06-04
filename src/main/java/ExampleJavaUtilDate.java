85542bceeb7a28710729da89c55c7c3c2d12271e
Jimmy Salazar <jimmy.salazar@tektonlabs.com>
2024-06-04 16:46:45 -0500

    feat: add date class

--- src/main/java/ExampleJavaUtilDate.java
+++ src/main/java/ExampleJavaUtilDate.java
@@ -1,7 +1,15 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ExampleJavaUtilDate {
     public static void main(String[] args) {
         Date date = new Date();
+        System.out.println("fecha = " + date);
+
+        // Formateador de fecha
+        SimpleDateFormat df = new SimpleDateFormat("dd-MMMM-YYYY"); // MM: Muestra el número de mes, MMMM: Muestra el nombre del mes, puede aceptar dd MMMM YYYY, dd/MMMM/YYYY
+        df = new SimpleDateFormat("EEEE dd 'de' MMMM, yyyy");
+        String fechaStr = df.format(date);
+        System.out.println("fecha str = " + fechaStr);
     }
 }
